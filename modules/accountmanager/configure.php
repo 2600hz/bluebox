cdd4f52db9d71925c35836a53a0dd51c797d72bc
k_anderson <k_anderson@23ae1789-3138-4bcd-9781-d22d15e76bf6>
2010-04-18 16:56:26 +0000

    enable on install
    git-svn-id: https://svn2.assembla.com/svn/bluebox@11 23ae1789-3138-4bcd-9781-d22d15e76bf6

--- modules/accountmanager/configure.php
+++ modules/accountmanager/configure.php
@@ -17,7 +17,7 @@ class AccountManager_Configure extends FreePbx_Configure
     public static $vendor = 'FreePBX';
     public static $license = 'MPL';
     public static $summary = 'Provides Account Management Tools';
-    public static $default = FALSE;
+    public static $default = true;
     public static $type = FreePbx_Installer::TYPE_MODULE;
     public static $required = array(
         'core' => 0.1
