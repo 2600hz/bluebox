8058d1c6503d8a91add0bb32d5b2a2a14c8b55ea
K Anderson <bitbashing@gmail.com>
2010-05-25 16:07:05 -0400

    FreePBX changeset 363: Patch up wording NOTICE: changeset 364 was already imported earlier and will not appear seperate

--- modules/freepbxmanager/views/freepbxmanager/maintenance.php
+++ modules/freepbxmanager/views/freepbxmanager/maintenance.php
@@ -11,8 +11,8 @@
             <label>Regenerate core configurations</label>
             <?php echo form::submit(array('name' => 'submit', 'class' => 'save small_green_button'), 'Regenerate Now'); ?>
         </div>
-        <p>Sometimes, the switch's configuration data can become out of sync with the database settings in your system. This can happen for a variety
-        of reasons ranging from bugs in the software to file permission issues. You can force a regeneration of core XML files automatically.</p>
+        <p>Sometimes configuration data can become out of sync with the database settings in your system. This can happen for a variety
+        of reasons ranging from bugs in the software to file permission issues. You can force a regeneration of core config files automatically.</p>
     </div>
 
     <?php echo form::close();?>
