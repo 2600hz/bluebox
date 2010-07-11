257d5f8c2f5af27bc638bb01bee866dc15375936
Darren Schreiber <d@d-man.org>
2010-07-11 15:14:32 -0700

    Put back the buttons on the context manager. Also, remove the outdated FS driver

--- modules/contextmanager-1.0/views/contextmanager/update.php
+++ modules/contextmanager-1.0/views/contextmanager/update.php
@@ -19,5 +19,13 @@
 
     <?php echo form::close_section(); ?>
 
+    <div class="buttons form_bottom">
+
+        <?php echo form::button(array('name' => 'submit', 'class' => 'cancel small_red_button'), 'Cancel'); ?>
+
+        <?php echo form::submit(array('name' => 'submit', 'class' => 'save small_green_button'), 'Save'); ?>
+
+    </div>
+    
     <?php echo form::close(); ?>
 </div>
