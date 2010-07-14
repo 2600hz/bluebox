823d3bdbb0eefbda4b6c9df1ff8595b19d795646
root <root@temp.switchfreedom.com>
2010-07-14 22:56:06 +0000

    change name to bluebox and cahnge some path into the "module/folder-1.0" folder

--- preinstall.sh
+++ preinstall.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-webuser='nobody'
+if [ -f /etc/redhat-release ]
+then
+	# Must be CentOS! Need a better check then this at some point.
+	webuser='nobody'
+
+else
+
+	# Assume Debian. This needs improvement.
+	webuser='www-data'
+
+fi
 
 # BlueBox Startup
 echo "Prepping for a typical BlueBox install. Your mileage may vary. It\'s OK if some things fail..."
@@ -19,10 +29,10 @@ chgrp -R $webuser bluebox/cache/
 chmod -R a+w bluebox/cache/
 chgrp -R $webuser bluebox/config/
 chmod -R a+w bluebox/config/
-chgrp -R $webuser modules/freeswitch/config/
-chmod -R a+w modules/freeswitch/config/
-chgrp -R $webuser modules/asterisk/config/
-chmod -R a+w modules/asterisk/config/
+chgrp -R $webuser modules/freeswitch-1.0/config/
+chmod -R a+w modules/freeswitch-1.0/config/
+chgrp -R $webuser modules/asterisk-1.0/config/
+chmod -R a+w modules/asterisk-1.0/config/
 mkdir -p upload/
 chgrp -R $webuser upload/
 chmod -R a+w upload/
