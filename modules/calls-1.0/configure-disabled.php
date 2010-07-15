955625a989690a41091e86d92cbb0a7d56c00b32
Darren Schreiber <d@d-man.org>
2010-07-15 13:32:07 -0700

    Adding calls to a new branch

---
+++ modules/calls-1.0/configure-disabled.php
@@ -0,0 +1,24 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+class Calls_Configure extends Bluebox_Configure
+{
+    public static $version = 0.1;
+    public static $packageName = 'calls';
+    public static $displayName = 'Call Reports';
+    public static $author = 'Karl Anderson';
+    public static $vendor = 'Bluebox';
+    public static $license = 'MPL';
+    public static $summary = 'Call Reporting';
+    public static $description = '';
+    public static $default = false;
+    public static $type = Bluebox_Installer::TYPE_MODULE;
+    public static $required = array(
+        'core' => 0.1
+    );
+    public static $navIcon = 'assets/img/icons/mainCallsX.png';
+    public static $navBranch = '/Reports/';
+    public static $navURL = 'calls/index';
+
+    public static function _checkExp() {
+        return array('warnings' => 'This module is experimental and not ready for production use!');
+    }
+}
