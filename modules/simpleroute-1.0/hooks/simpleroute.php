ba2f074ecfbd9ab6a42d75ff28b31f21c729dbbe
K Anderson <bitbashing@gmail.com>
2010-07-17 20:41:08 -0400

    update the simpleroute hook

--- modules/simpleroute-1.0/hooks/simpleroute.php
+++ modules/simpleroute-1.0/hooks/simpleroute.php
@@ -1,9 +1,13 @@
-<?php
-// Add routing support to trunks
-plugins::register('trunkmanager/create', 'view', array('SimpleRoute_Plugin', 'update'));
-plugins::register('trunkmanager/edit', 'view', array('SimpleRoute_Plugin', 'update'));
-plugins::register('trunkmanager/index', 'view', array('SimpleRoute_Plugin', 'index'));
+<?php defined('SYSPATH') or die('No direct access allowed.');
 
-// Handle saves
-plugins::register('trunkmanager', 'save', array('SimpleRoute_Plugin', 'save'));
-plugins::register('trunkmanager', 'delete', array('SimpleRoute_Plugin', 'delete'));
+    // Add routing support to trunks
+    plugins::register('trunkmanager/create', 'view', array('SimpleRoute_Plugin', 'update'));
+
+    plugins::register('trunkmanager/edit', 'view', array('SimpleRoute_Plugin', 'update'));
+
+    plugins::register('trunkmanager/index', 'view', array('SimpleRoute_Plugin', 'index'));
+
+    // Handle saves
+    plugins::register('trunkmanager', 'save', array('SimpleRoute_Plugin', 'save'));
+    
+    plugins::register('trunkmanager', 'delete', array('SimpleRoute_Plugin', 'delete'));
