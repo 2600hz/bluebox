66ae8562471dcd5b1feca5d5b6b6215d3914f56b
Dale Hege <fdhege@gmail.com>
2010-07-18 00:05:26 -0400

    more code for importing call logs

--- modules/calls-1.0/libraries/ProcessLog.php
+++ modules/calls-1.0/libraries/ProcessLog.php
@@ -7,25 +7,57 @@
  */
 class ProcessLog {
 
+
     public function importLogs() {
         // Scan for new logs that haven't been processed before and import them into the log table
         // Make sure not to get dupes, even if there's a crash (how?)
 
+       $coreFields = array(
+            'uuid' => 1, 
+            'accountcode' => 1,
+            'caller_id_number' => 1,
+            'destination_number' => 1,
+            'context' => 1,
+            'duration' => 1,
+            'start_stamp' => 1,
+            'answer_stamp' => 1,
+            'end_stamp' => 1,
+            'billsec' => 1,
+            'hangup_cause' => 1,
+            'channel_name' => 1,
+            'bridge_channel' => 1
+        );
+
         $recordCount = 0;
 
-        $callFiles = glob(rtrim(Kohana::config('calls.scandir'), '/') . '/' . 'Master.csv.*', GLOB_MARK);
         Kohana::log('debug', 'scandir: ' . Kohana::config('calls.scandir'));
+
+        $callFiles = glob(rtrim(Kohana::config('calls.scandir'), '/') . '/' . 'Master.csv.*', GLOB_MARK);
+
         foreach( $callFiles as $callFile ) {
             if (($callFileHandle = fopen($callFile, "r")) !== FALSE) {
                 while (($callRecord = fgetcsv($callFileHandle, 2000, ",")) !== FALSE) {
+
+                    $corecdr = array();
+                    $extracdr = array();
+
                     $numFields = count($callRecord);
                     $recordCount++;
-                    Kohana::log('debug'. "$numFields fields in record $recordCount");
-                    $cdr=array();
-                    for ($field=0; $field < $numFields; $field=$field+2) {
-                        $cdr[$callRecord[$field]]=$callRecord[$field+1];
+
+                    $insertCall = new Calls;
+
+                    Kohana::log('debug', "$numFields fields in record $recordCount");
+
+                    for ($field = 0; $field < $numFields; $field = $field+2) {
+                        if(isset($coreFields[$callRecord[$field]])) {
+                            $corecdr[$callRecord[$field]] = $callRecord[$field+1];
+                       } else {
+                            $extracdr[$callRecord[$field]] = $callRecord[$field+1];
+                       }
                     }
-                    Kohana::log('debug', 'Call Log: ' . print_r($cdr,true));
+                    Kohana::log('debug', 'Call Log: ' . print_r($corecdr,true));
+                    Kohana::log('debug', 'Call Log: ' . print_r($extracdr,true));
+                    
                 }
                 fclose($callFileHandle);
             }
