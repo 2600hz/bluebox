8d7957f4c1e19500ad98e346afd86576474008ce
Darren Schreiber <d@d-man.org>
2010-07-20 18:38:49 -0700

    Add more sample for James

--- modules/featurecode-1.0/views/featurecode/update.php
+++ modules/featurecode-1.0/views/featurecode/update.php
@@ -22,12 +22,68 @@
 
     <?php echo form::open_section('FreeSWITCH XML'); ?>
 
-        <div class="field assign_numbers">
+        <div class="field">
         <?php
-            echo form::label('featurecode[xml]', 'XML:');
-            echo form::textarea('featurecode[xml]');
+            echo form::label(array(
+                'for' => 'net_xml',
+                'hint' => 'Network related XML',
+                'help' => 'Add XML that changes network-related call properties.'
+                ),
+                'Add Net XML?'
+            );
+            echo form::checkbox(array('class' => 'determinant agent_for_netXml',  'name' => 'net_xml'));
         ?>
         </div>
+
+        <div class="field">
+        <?php
+            echo form::label('featurecode[netXml]', 'Network XML:');
+            echo form::textarea(array('name' => 'featurecode[netXml]', 'class' => 'dependent_positive rely_on_netXml'));
+        ?>
+        </div>
+
+
+        <div class="field">
+        <?php
+            echo form::label(array(
+                'for' => 'condition_xml',
+                'hint' => 'Conditioning XML',
+                'help' => 'Add XML that conditions FreeSWITCH and call variables.'
+                ),
+                'Add Conditioning XML?'
+            );
+            echo form::checkbox(array('class' => 'determinant agent_for_conditionXml',  'name' => 'condition_xml'));
+        ?>
+        </div>
+
+        <div class="field">
+        <?php
+            echo form::label('featurecode[conditionXml]', 'Condition XML:');
+            echo form::textarea(array('name' => 'featurecode[conditionXml]', 'class' => 'dependent_positive rely_on_conditionXml'));
+        ?>
+        </div>
+
+
+        <div class="field">
+        <?php
+            echo form::label(array(
+                'for' => 'preroute_xml',
+                'hint' => 'Pre-routing XML',
+                'help' => 'Add XML that makes routing decisions right off the bat.'
+                ),
+                'Add Pre-Route XML?'
+            );
+            echo form::checkbox(array('class' => 'determinant agent_for_prerouteXml',  'name' => 'preroute_xml'));
+        ?>
+        </div>
+
+        <div class="field">
+        <?php
+            echo form::label('featurecode[prerouteXml]', 'Pre-Route XML:');
+            echo form::textarea(array('name' => 'featurecode[netXml]', 'class' => 'dependent_positive rely_on_prerouteXml'));
+        ?>
+        </div>
+
 
     <?php echo form::close_fieldset(); ?>
 
