5cb2b8efe454bc8afb3a475c44d4540edda01be8
K Anderson <bitbashing@gmail.com>
2010-07-22 16:42:20 -0400

    if the no-answer is trnsfr then continue on fail, and hangup on answer

--- modules/freeswitch-1.0/libraries/drivers/freeswitch/numbercontext.php
+++ modules/freeswitch-1.0/libraries/drivers/freeswitch/numbercontext.php
@@ -53,9 +53,24 @@ class FreeSwitch_NumberContext_Driver extends FreeSwitch_Base_Driver {
             // Now that the extension and condition fields are created for this number, set our root to inside the condition
             $xml->setXmlRoot($xml->getXmlRoot() . '/condition[@field="destination_number"][@expression="^' .$base['Number']['number'] . '$"]');
 
+            $dialplan = $base['Number']['dialplan'];
+
             // Add an extension-specific prenumber items
             // Note that unlike other dialplan adds, this one assumes you're already in the right spot in the XML document for the add
             dialplan::preNumber($base['Number']);
+
+            if (!empty($dialplan['terminate']['action']))
+            {
+                switch($dialplan['terminate']['action'])
+                {
+                    case 'transfer':
+                        $xml->update('/action[@application="set"][@bluebox="settingEndBridge"][@data="hangup_after_bridge=true"]');
+
+                        $xml->update('/action[@application="set"][@bluebox="settingFail"][@data="continue_on_fail=true"]');
+
+                        break;
+                }
+            }
 
             // Add related final destination XML
             $destinationDriverName = Telephony::getDriverName() .'_' .substr($base['Number']['class_type'], 0, strlen($base['Number']['class_type']) - 6) .'_Driver';
@@ -78,8 +93,6 @@ class FreeSwitch_NumberContext_Driver extends FreeSwitch_Base_Driver {
             // Add an anti-action / failure route for this dialplan
             // Note that unlike other dialplan adds, this one assumes you're already in the right spot in the XML document for the add
             dialplan::postNumber($base['Number']);
-
-            $dialplan = $base['Number']['dialplan'];
 
             if (!empty($dialplan['terminate']['action']))
             {
