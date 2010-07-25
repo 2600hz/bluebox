6b4caf45537fb380b9ef4f87dfe428a90d638266
K Anderson <BitBashing@GMail.com>
2010-07-25 16:50:06 -0400

    adding in i18n support

--- modules/devicemanager-1.0/views/devicemanager/numberOptions.mus.php
+++ modules/devicemanager-1.0/views/devicemanager/numberOptions.mus.php
@@ -2,13 +2,15 @@
 
     <div class="field">
 
-        <label for="number{{number_id}}_registry_ringtype" class="label" id="label_number{{number_id}}_registry_ringtype">Ring Type: </label>
+        <label for="number{{number_id}}_registry_ringtype" class="label" id="label_number{{number_id}}_registry_ringtype">
+            <?php echo __('Ring Type'); ?>:
+        </label>
 
         <select class="dropdown" name="number{{number_id}}[registry][ringtype]" id="number{{number_id}}_registry_ringtype">
 
-            <option value="ringing" {{#ringtype_ringing}}selected="selected"{{/ringtype_ringing}}>Ringing</option>
+            <option value="ringing" {{#ringtype_ringing}}selected="selected"{{/ringtype_ringing}}><?php echo __('Ringing'); ?></option>
 
-            <option value="moh" {{#ringtype_moh}}selected="selected"{{/ringtype_moh}}>Hold Music</option>
+            <option value="moh" {{#ringtype_moh}}selected="selected"{{/ringtype_moh}}><?php echo __('Hold Music'); ?></option>
 
         </select>
 
@@ -16,7 +18,9 @@
 
     <div class="field">
 
-        <label for="number{{number_id}}_registry_timeout" class="label" id="label_number{{number_id}}_registry_timeout">Ring this device for:</label>
+        <label for="number{{number_id}}_registry_timeout" class="label" id="label_number{{number_id}}_registry_timeout">
+            <?php echo __('Ring this device for'); ?>:
+        </label>
 
         <input type="text" value="{{timeout}}" class="input" name="number{{number_id}}[registry][timeout]" id="number{{number_id}}_registry_timeout" /> seconds
 
@@ -24,7 +28,9 @@
 
     <div class="field">
 
-        <label for="number{{number_id}}_registry_ignore_fwd" class="label" id="label_number{{number_id}}_registry_ignore_fwd">Disable Call Forwarding:</label>
+        <label for="number{{number_id}}_registry_ignore_fwd" class="label" id="label_number{{number_id}}_registry_ignore_fwd">
+            <?php echo __('Disable Call Forwarding'); ?>:
+        </label>
 
         <input type="checkbox" value="1" {{#ignoreFWD}}checked="checked"{{/ignoreFWD}} class="input" name="number{{number_id}}[registry][ignoreFWD]" id="number{{number_id}}_registry_ignore_fwd" />
 
