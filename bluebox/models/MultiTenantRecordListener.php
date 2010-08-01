3d5c112d22c527a23af184959728f8501679cc58
K Anderson <bitbashing@gmail.com>
2010-08-01 16:38:05 -0400

    complete refactor to allow the disassociate the listner from always being the users account_id, and better test for the Auth module

--- bluebox/models/MultiTenantRecordListener.php
+++ bluebox/models/MultiTenantRecordListener.php
@@ -11,41 +11,87 @@
  * @author Darren Schreiber <d@d-man.org>
  * @license Closed/Copyright Darren Schreiber
  */
-
 class MultiTenantRecordListener extends Doctrine_Record_Listener
 {
-    /** Handle object references **/
+    private $account_id = 0;
+
+    public function getUserId()
+    {
+        if (empty($this->account_id))
+        {
+            // The user may change for example force_login
+            if (!empty(users::$user['account_id']))
+            {
+                $this->account_id = users::$user['account_id'];
+            }
+
+            $session = Session::instance();
+
+            $account_id = $session->get('multitenant_account_id', FALSE);
+
+            if (!empty($account_id))
+            {
+                if (request::is_ajax())
+                {
+                    $controller = $session->get('ajax.base_controller', NULL);
+                }
+                else
+                {
+                    $controller = strtolower(Router::$controller);
+                }
+
+                if ($controller == 'accountmanager')
+                {
+                       $this->account_id = $account_id;
+                }
+                else
+                {
+                    $session->delete('multitenant_account_id');
+                }
+            }
+
+            if (empty($this->account_id))
+            {
+                Kohana::log('debug', 'Throwing exception due to empty user account_id');
+
+                throw new Exception('Unable to determine your authorization to manipulate this record');
+            }
+
+            kohana::log('debug', 'MultiTenantRecordListener is using account id ' .$this->account_id);
+        }
+
+        return $this->account_id;
+    }
+
     public function preSave(Doctrine_Event $event)
     {
         $record = &$event->getInvoker();
 
-        if (!isset($record['account_id'])) {
-            Kohana::log('debug', 'Throwing exception due to unset account_id - preSave');
-            throw new Exception('Unable to assign this record to your account', 1);
+        if (!isset($record['account_id']))
+        {
+            Kohana::log('debug', 'Throwing exception due to unset account_id');
+
+            throw new Exception('Unable to assign this record to your account');
         }
 
-        if (!empty(users::$user['account_id'])) {
-            $record['account_id'] = users::$user['account_id'];
-        } else {
-            Kohana::log('debug', 'Throwing exception due to empty user account_id - preSave');
-            throw new Exception('Unable to determine your authorization to add this record', 1);
-        }
+        $record['account_id'] = $this->getUserId();
     }
 
     public function preUpdate(Doctrine_Event $event)
-    {
+    {   
         $record = &$event->getInvoker();
 
-        if (!isset($record['account_id'])) {
-            Kohana::log('debug', 'Throwing exception due to unset account_id - preUpdate');
-            throw new Exception('Unable to assign this record to your account', 1);
+        if (!isset($record['account_id']))
+        {
+            Kohana::log('debug', 'Throwing exception due to unset account_id');
+
+            throw new Exception('Unable to assign this record to your account');
         }
 
-        if (empty(users::$user['account_id'])) {
-            Kohana::log('debug', 'Throwing exception due to empty user account_id - preUpdate');
-            throw new Exception('Unable to determine your authorization to update this record', 1);
-        } else if ($record['account_id'] != users::$user['account_id']) {
-            Kohana::log('debug', 'Throwing exception account_id mismatch - preUpdate');
+        if ($record['account_id'] != $this->getUserId())
+        {
+            Kohana::log('debug', 'Throwing exception account_id mismatch, ' .$record['account_id'] .' != ' .$this->getUserId());
+
             throw new Exception('You do not have authorization to update this record', 1);
         }
     }
@@ -54,17 +100,14 @@ class MultiTenantRecordListener extends Doctrine_Record_Listener
     {
         $record = &$event->getInvoker();
 
-        if (!isset($record['account_id'])) {
-            Kohana::log('debug', 'Throwing exception due to unset account_id - preInsert');
-            throw new Exception('Unable to assign this record to your account', 1);
+        if (!isset($record['account_id']))
+        {
+            Kohana::log('debug', 'Throwing exception due to unset account_id');
+
+            throw new Exception('Unable to assign this record to your account');
         }
 
-        if (!empty(users::$user['account_id'])) {
-            $record['account_id'] = users::$user['account_id'];
-        } else {
-            Kohana::log('debug', 'Throwing exception due to empty user account_id - preInsert');
-            throw new Exception('Unable to determine your authorization to add this record', 1);
-        }
+        $record['account_id'] = $this->getUserId();
     }
 
     public function preDelete(Doctrine_Event $event)
@@ -72,49 +115,51 @@ class MultiTenantRecordListener extends Doctrine_Record_Listener
         $record = &$event->getInvoker();
 
         // This is dangerous - should we be more strict?
-        if (empty($record['account_id'])) {
-            Kohana::log('debug', 'Throwing exception due to unset account_id - preInsert');
-            throw new Exception('Unable to determine the account owner of this record', 1);
+        if (empty($record['account_id']))
+        {
+            Kohana::log('debug', 'Throwing exception due to unset account_id');
+
+            throw new Exception('Unable to determine the account owner of this record');
         }
 
-        if (empty(users::$user['account_id'])) {
-            Kohana::log('debug', 'Throwing exception due to empty user account_id - preDelete');
-            throw new Exception('Unable to determine your authorization to delete this record', 1);
-        }
+        if ($record['account_id'] != $this->getUserId())
+        {
+            Kohana::log('debug', 'Throwing exception account_id mismatch, ' .$record['account_id'] .' != ' .$this->getUserId());
 
-        if ($record['account_id'] != users::$user['account_id']) {
-            Kohana::log('debug', 'Throwing exception account_id mismatch - preDelete');
-            throw new Exception('You do not have authorization to delete this record', 1);
+            throw new Exception('You do not have authorization to update this record', 1);
         }
     }
 
-    /** Handle DQL **/
     public function preDqlUpdate($event)
-    {
+    {   
         $q = $event->getQuery();
-        $q->andWhere('account_id = ' .users::$user['account_id']);
+
+        $q->andWhere('account_id = ' .$this->getUserId());
     }
 
     public function preDqlSelect($event)
     {
-        // HACK! TODO: Fix this
-        // For now, if there's no authenticated user, don't filter on any user for selects
-        if ((!users::$user) or (!users::$user['account_id'])) {
+        $query = $event->getQuery();
+
+        $from = $query->getDqlPart('from');
+
+        // This allows the auth module to select users, regardless of the account
+        if ($from = 'User dctrn_find' AND empty(users::$user['account_id']))
+        {
+            kohana::log('debug', 'Skipping preDqlSelect for the user auth');
+
             return;
         }
 
         $q = &$event->getQuery();
-        $q->andWhere('account_id = ' .users::$user['account_id']);
+
+        $q->andWhere('account_id = ' .$this->getUserId());
     }
 
     public function preDqlDelete($event)
     {
         $q = &$event->getQuery();
-        $q->andWhere('account_id = ' .users::$user['account_id']);
-    }
 
-    public function preValidate($event)
-    {
-
+        $q->andWhere('account_id = ' .$this->getUserId());
     }
 }
