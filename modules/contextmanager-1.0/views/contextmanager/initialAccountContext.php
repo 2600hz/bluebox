143bebb615cdd56a4e4b6d55028d93fc189b792b
K Anderson <bitbashing@gmail.com>
2010-08-01 16:40:28 -0400

    support the account manager during new account creation

---
+++ modules/contextmanager-1.0/views/contextmanager/initialAccountContext.php
@@ -0,0 +1,21 @@
+<?php echo form::open_section('Context Information'); ?>
+
+    <div class="field">
+    <?php
+        echo form::label('context[private]', 'Create Private Context:');
+        echo form::checkbox('context[private]', NULL, isset($context['private']) ? $context['private'] : TRUE);
+        echo ' called ';
+        echo form::input('context[private_name]', empty($context['private_name']) ? 'In-house Only' : $context['private_name']);
+    ?>
+    </div>
+
+    <div class="field">
+    <?php
+        echo form::label('context[public]', 'Create Public Context:');
+        echo form::checkbox('context[public]', NULL, isset($context['public']) ? $context['public'] : TRUE);
+        echo ' called ';
+        echo form::input('context[public_name]', empty($context['public_name']) ? 'Publicly Accessible' : $context['public_name']);
+    ?>
+    </div>
+
+<?php echo form::close_section(); ?>
