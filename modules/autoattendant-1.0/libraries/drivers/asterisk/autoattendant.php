41fad06fc40db3bc63b569b58ff0cf5f53812dec
K Anderson <BitBashing@GMail.com>
2010-08-02 05:39:51 -0400

    update the auto-attendant driver

--- modules/autoattendant-1.0/libraries/drivers/asterisk/autoattendant.php
+++ modules/autoattendant-1.0/libraries/drivers/asterisk/autoattendant.php
@@ -1,52 +1,93 @@
-<?php
+<?php defined('SYSPATH') or die('No direct access allowed.');
 
-class Asterisk_AutoAttendant_Driver extends Asterisk_Base_Driver {
-    /**
-     * Indicate we support Asterisk for Auto Attendants
-     */
-    public static function set($obj)
+class Asterisk_AutoAttendant_Driver extends Asterisk_Base_Driver
+{
+    public static function set($autoattendant)
     {
+
     }
 
-    public static function delete($obj)
+    public static function delete($autoattendant)
     {
+        
     }
 
-    public static function dialplan($obj)
+    public static function dialplan($number)
     {
+        $autoattendant = $number['Destination'];
+
         $doc = Telephony::getDriver()->doc;
 
         $doc->setCurrentNumber('i');
+
         $doc->add('Return', 1);
 
         $doc->setCurrentNumber('t');
+
         $doc->add('Return', 1);
 
         $doc->setCurrentNumber('h');
+
         $doc->add('Return', 1);
 
-        foreach ($obj->AutoAttendant->AutoAttendantKey as $key) {
-            $doc->setCurrentNumber($key->auto_attendant_key);
-            $doc->add('Goto(number_' . $key->number_id . ',' .$key->Number->number .',1)',1);
+        foreach ($autoattendant['keys'] as $key)
+        {
+            if (empty($key['digits']))
+            {
+                continue;
+            }
+
+            if($transfer = astrsk::getTransferToNumber($key['number_id']))
+            {
+                $doc->setCurrentNumber($key['digits']);
+
+                $doc->add('Goto(' .$transfer .')', 1);
+            }
         }
 
         // Don't forget to set the position back to where it was before
         $doc->setCurrentNumber('_X.');
 
         $doc->add('Answer', 1);
-        $doc->add('Set(TIMEOUT(digit)=' . $obj->AutoAttendant->digit_timeout . ')'); //TODO
-        $doc->add('Set(TIMEOUT(response)=' . $obj->AutoAttendant->timeout . ')'); //TODO
 
-        $doc->add('Set(f=3)');  // Initialize counter
-        // TODO: implement "say" command in Asterisk land
-        if ($obj->AutoAttendant->file_id) {
-            $doc->add('Background("' . FileManager::getFilePath($obj->AutoAttendant->file_id) . '")', 'REPEAT');
-        } else {
-            $doc->add('WaitExten(20)', 'REPEAT');
+        $doc->add('Set(TIMEOUT(digit)=' .$autoattendant['digit_timeout'] . ')');
+
+        $doc->add('Set(TIMEOUT(response)=' .$autoattendant['timeout'] . ')');
+
+        if (!empty($autoattendant['registry']['max-failures']))
+        {
+            $doc->add('Set(f=' .$autoattendant['registry']['max-failures'] .')');
         }
-        $doc->add('WaitExten(5)');
-        $doc->add('Set(f=$[${f} - 1])');
-        $doc->add('GotoIf($[${f} > 0]?REPEAT)');
+        else
+        {
+            $doc->add('Set(f=3)');
+        }
 
+        if (empty($autoattendant['registry']['mediafile_id']) OR !class_exists('Media'))
+        {
+            $type = 'tty';
+        }
+        else
+        {
+            $type = $autoattendant['registry']['type'];
+        }
+
+        switch($type)
+        {
+            case 'audio':
+
+                $doc->add('Background("' . Media::getFilePath($autoattendant['registry']['mediafile_id']) . '")', 'REPEAT');
+
+                break;
+
+            default:
+                $doc->add('WaitExten(5)', 'REPEAT');
+        }
+
+        $doc->add('WaitExten(5)');
+
+        $doc->add('Set(f=$[${f} - 1])');
+
+        $doc->add('GotoIf($[${f} > 0]?REPEAT)');
     }
 }
