502fa09f2ef59c018c5b6ce826e86ecf17d02163
dschreiber <d@d-man.org>
2010-08-21 02:42:20 -0700

    Sample adds on install

--- modules/endpointmanager-1.0/configure.php
+++ modules/endpointmanager-1.0/configure.php
@@ -16,7 +16,7 @@ class EndpointManager_1_0_Configure extends Bluebox_Configure
         'core' => 0.1
     );
     public static $navLabel = 'Endpoint Manager';
-    public static $navBranch = '/Application/';
+    public static $navBranch = '/Applications/';
     public static $navURL = 'endpointmanager/index';
     public static $navSubmenu = array(
         'List' => 'endpointmanager/index',
@@ -30,4 +30,33 @@ class EndpointManager_1_0_Configure extends Bluebox_Configure
             'disabled' => true
         )
     );
+
+    public function postInstall() {
+        $brand = new EndpointBrand();
+        $brand['name'] = 'Aastra';
+        $brand['directory'] = '';
+        $brand['cfg_ver'] = '';
+        $brand['endpoint_brand_id'] = 1;
+        $brand->save();
+
+        $productLine = new EndpointProductLine();
+        $productLine->EndpointBrand = $brand;
+        $productLine['short_name'] = 'aap9xxx6xxx';
+        $productLine['long_name'] = '9xxx and 6xxx line';
+        $productLine['endpoint_product_line_id'] = 1;
+        $productLine['cfg_dir'] = '';
+        $productLine['cfg_ver'] = '';
+        $productLine['firmware_vers'] = '';
+        $productLine['firmware_files'] = '';
+        $productLine['special_cfgs'] = '';
+        $productLine->save();
+
+        $model = new EndpointModel();
+        $model->EndpointProductLine = $productLine;
+        $model['name'] = '6755i';
+        $model['template_list'] = '';
+        $model['template_data'] = '';
+        $model['endpoint_model_id'] = 1;
+        $model->save();
+    }
 }
