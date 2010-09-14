9a0a023e45a1832fd6366db151e7d575b22ce687
tm1000 <andrew.nagy@the159.com>
2010-09-13 21:04:45 -0700

    Fixed Endpoint Manager Hooks

--- modules/endpointmanager-1.0/hooks/endpointmanager.php
+++ modules/endpointmanager-1.0/hooks/endpointmanager.php
@@ -1,7 +1,2 @@
 <?php
-
-plugins::register('devicemanager/create', 'view', array('EndpointManager_Plugin', 'update'));
-
-plugins::register('devicemanager/edit', 'view', array('EndpointManager_Plugin', 'update'));
-
-plugins::register('devicemanager', 'save', array('EndpointManager_Plugin', 'save'));
+?>
