1752191f88d34d4bf097a2f8d11edca922e6665a
K Anderson <BitBashing@GMail.com>
2010-10-17 21:08:41 -0400

    set up a relation with application delete cascades defined and preload the model on account removal

--- modules/simpleroute-1.1/hooks/simpleroute.php
+++ modules/simpleroute-1.1/hooks/simpleroute.php
@@ -6,4 +6,6 @@
 
     plugins::register('trunkmanager', 'save', array('SimpleRoute_Plugin', 'save'));
 
+    plugins::register('accountmanager', 'delete', array('SimpleRoute_Plugin', 'buildAccountRelationships'));
+
     Event::add('bluebox.save_prepare', array('SimpleRouteLib', 'importRoutesNewAccount'));
