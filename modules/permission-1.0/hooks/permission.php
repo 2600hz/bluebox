12de367e088971c2bf85e8ca28e53beeedf606ed
K Anderson <BitBashing@GMail.com>
2010-10-27 00:20:57 -0400

    BLUEBOX-25

--- modules/permission-1.0/hooks/permission.php
+++ modules/permission-1.0/hooks/permission.php
@@ -2,7 +2,7 @@
 
 if (!Bluebox_Installer::is_installing()) {
     // This is hook enforces controller enable/disable permissions
-    Event::add('system.post_routing', array(
+    Event::add('system.post_controller_constructor', array(
         'Permission_Plugin',
         'bootstrapPermission'
     ));
