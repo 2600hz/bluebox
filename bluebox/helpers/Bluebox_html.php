81b2f07120aa90821710f1581e8432aae6faad24
K Anderson <BitBashing@GMail.com>
2010-11-08 04:54:05 -0500

    refactor image to be more readable, and added a token method that will create HTML compliant id/name tokens from a string

--- bluebox/helpers/Bluebox_html.php
+++ bluebox/helpers/Bluebox_html.php
@@ -44,4 +44,15 @@ class html extends html_Core
 
         return parent::image($attributes, $alt, $index);
     }
+
+    public static function token($string)
+    {
+        $string = preg_replace('/[^a-zA-Z0-9-_:.]+/imx', '_', $string);
+
+        $string = trim($string, '_');
+
+        // TODO: ensure it starts with a letter!
+
+        return strtolower($string);
+    }
 }
