5622d2b6fa81bdb3a41278378365a671fc4a4e67
K Anderson <BitBashing@GMail.com>
2010-11-08 13:38:07 -0500

    sane defaults

--- modules/maintenance-1.0/config/maintenance.php
+++ modules/maintenance-1.0/config/maintenance.php
@@ -1,5 +1,5 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');
 
-$config['cycle_time'] = 0; //sec
+$config['cycle_time'] = 300; //sec
 
-$config['on_requests'] = FALSE;
+$config['on_requests'] = TRUE;
