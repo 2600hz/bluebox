a5e5146a902cfec5139e99b3199a4c5d5e55c3b6
K Anderson <BitBashing@GMail.com>
2010-11-14 01:07:11 -0500

    inappropriately moved the generatePassword into inflector but there isnt a better place for it right now...

--- modules/sip-1.0/libraries/SipLib.php
+++ modules/sip-1.0/libraries/SipLib.php
@@ -8,35 +8,9 @@ class SipLib
 
         $plugin = array('sip' => array(
             'username' => html::token($device['name']),
-            'password' => self::generatePassword()
+            'password' => inflector::generatePassword()
         ));
 
         $device['plugins'] = arr::merge($device['plugins'], $plugin);
-    }
-
-    public static function generatePassword($length = 8)
-    {
-        $consonants = 'bdghjmnpqrstvz';
-        $consonants .= 'BDGHJLMNPQRSTVWXZ';
-        $consonants .= '1234567890';
-
-        $vowels = 'aeuy';
-        $vowels .= "AEUY";
-
-        $password = '';
-        $alt = time() % 2;
-        for ($i = 0; $i < $length; $i++) {
-                if ($alt == 1) {
-                        $password .= $consonants[(rand() % strlen($consonants))];
-                        $alt = 0;
-                } else {
-                        $password .= $vowels[(rand() % strlen($vowels))];
-                        $alt = 1;
-                }
-        }
-        // at least one digit
-        $password = substr_replace($password, rand(0, 9), rand(1, $length), 1);
-
-        return $password;
     }
 }
