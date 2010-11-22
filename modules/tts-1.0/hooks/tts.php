596a92c37f6987a44dfad3b04863fa28b0006313
K Anderson <BitBashing@GMail.com>
2010-11-22 17:26:41 -0500

    generate configuration in a modular way

--- modules/tts-1.0/hooks/tts.php
+++ modules/tts-1.0/hooks/tts.php
@@ -1,3 +1,5 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');
 
     Event::add('bluebox.media.widget', array('TextToSpeech', 'provideMediaWidget'));
+
+    Event::add('bluebox.media.text_to_speech', array('TextToSpeech', 'generateConfiguration'));
