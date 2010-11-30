f1408a40c1cf64ecd18b27d89a64f7b279375723
K Anderson <BitBashing@GMail.com>
2010-11-30 10:57:20 -0500

    remove this for now, it is causing: Commit failed. There is no active transaction.

--- bluebox/controllers/bluebox.php
+++ bluebox/controllers/bluebox.php
@@ -856,8 +856,6 @@ abstract class Bluebox_Controller extends Template_Controller
 
     protected function save_prepare(&$object)
     {
-        Doctrine_Manager::connection()->beginTransaction();
-        
         // Let things know we are about to save
         Event::run('bluebox.save_prepare', $object);
     }
@@ -876,16 +874,12 @@ abstract class Bluebox_Controller extends Template_Controller
 
     protected function save_succeeded(&$object)
     {
-        Doctrine_Manager::connection()->commit();
-
         // Let things respond to a failed save
         Event::run('bluebox.save_succeeded', $object);
     }
 
     protected function save_failed(&$object)
     {
-        Doctrine_Manager::connection()->rollback();
-
         // Let things respond to a failed save
         Event::run('bluebox.save_failed', $object);
     }
