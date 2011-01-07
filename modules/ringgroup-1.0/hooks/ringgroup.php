b820097d0fb9fd83579ad7ac22a44ea11d74048a
K Anderson <BitBashing@GMail.com>
2011-01-06 23:00:55 -0500

    allow the ringgroup to have number options

--- modules/ringgroup-1.0/hooks/ringgroup.php
+++ modules/ringgroup-1.0/hooks/ringgroup.php
@@ -1,4 +1,6 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');
 
+    Event::add('numbermanager.collectNumberOptions', array('RingGroups', 'provideNumberOptions'));
+
     Event::add('numbermanager.collectNumberTargets', array('RingGroups', 'provideNumberTargets'));
 
