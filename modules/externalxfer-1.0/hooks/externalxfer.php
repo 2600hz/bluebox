f54de087882419a72453a89e0193e08a11b8dbe5
dschreiber <d@d-man.org>
2011-02-28 09:19:38 -0500

    Remove external transfer module, its not complete yet

--- modules/externalxfer-1.0/hooks/externalxfer.php
+++
@@ -1,5 +0,0 @@
-<?php defined('SYSPATH') or die('No direct access allowed.');
-
-    Event::add('numbermanager.collectNumberOptions', array('ExternalXfers', 'provideNumberOptions'));
-    
-    Event::add('numbermanager.collectNumberTargets', array('ExternalXfers', 'provideNumberTargets'));
