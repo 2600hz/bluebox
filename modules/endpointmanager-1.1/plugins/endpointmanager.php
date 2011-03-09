9fd8d33f58d234c6c1eddf6f672aa9c4f59e5e46
dschreiber <d@d-man.org>
2011-03-09 05:52:32 -0800

    Putting this back the way it was, Im stupid

--- modules/endpointmanager-1.1/plugins/endpointmanager.php
+++ modules/endpointmanager-1.1/plugins/endpointmanager.php
@@ -42,13 +42,11 @@ class EndpointManager_Plugin extends Bluebox_Plugin
 			$phone_info['line'][1]['options']['outbound_proxy_server_port'] = (($port = arr::get($device, 'plugins', 'endpointdevice', 'proxy_port')) ? $port : 5060);
 
 			$vlan_enable = arr::get($device, 'plugins', 'endpointdevice', 'vlan');
-			$voice_vlan = arr::get($device, 'plugins', 'endpointdevice', 'voice_vlan');
-			$data_vlan = arr::get($device, 'plugins', 'endpointdevice', 'data_vlan') ? $data_vlan : '4095');
 
 			$phone_info['options']['voice_vlan_enable'] = ($vlan_enable ? $vlan_enable : '0');
-			$phone_info['options']['voice_vlan_id'] = ($voice_vlan ? $voice_vlan : '0');
+			$phone_info['options']['voice_vlan_id'] = (($voice_vlan = arr::get($device, 'plugins', 'endpointdevice', 'voice_vlan')) ? $voice_vlan : '0');
 			$phone_info['options']['data_vlan_enable'] = ($vlan_enable ? $vlan_enable : '0');
-			$phone_info['options']['data_vlan_id'] = ($data_vlan ? $data_vlan : '4095');
+			$phone_info['options']['data_vlan_id'] = ((arr::get($device, 'plugins', 'endpointdevice', 'data_vlan') ? $data_vlan : '4095');
 		} else {
 	                $phone_info['line'][1]['options'] = array();
 		}
