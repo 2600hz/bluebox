2ed355a748406b98a99ae50688fa97acc65fb098
Rob Hutton <justlikeef@gmail.com>
2011-04-11 11:02:23 -0400

    Breaks out the file handling from the presentation logic so that the file name can be retreived by other components that may want to allow playback of recorded calls.

--- modules/callrecord-1.0/controllers/callrecord.php
+++ modules/callrecord-1.0/controllers/callrecord.php
@@ -10,12 +10,34 @@ class CallRecord_Controller extends Bluebox_Controller {
             Kohana::log('error', 'Can\'t access file: '  . $file);
             return;
         }
+        $this->listenFile($file);
+    }
 
+    public function listenFile($filename)
+    {
         header("Content-type: audio/wav");
-        header('Content-Length: ' . filesize($file));
-        readfile($file);
+        header('Content-Length: ' . filesize($filename));
+        readfile($filename);
         die();
     }
 
+    public function get($uuid) {
+        $this->auto_render = FALSE;
 
+        $file = CallRecord::getFile($uuid);
+        if(!file_exists($file)) {
+            Kohana::log('error', 'Can\'t access file: '  . $file);
+            return;
+        }
+        $this->getFile($file);
+    }
+
+    public function getFile($filename)
+    {
+        header('Content-type: audio/wav');
+        header('Content-Length: ' . filesize($filename));
+        header('Content-disposition: attachment; filename="' . basename($filename) . '"');
+        readfile($filename);
+        die();
+    }
 }
