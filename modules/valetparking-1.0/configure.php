32ae5bd9bd59cc4c06de03742b63512b3ac7fcaa
Rob Hutton <justlikeef@gmail.com>
2011-05-26 15:51:59 -0400

    Add space to Valet Parking navigation label

--- modules/valetparking-1.0/configure.php
+++ modules/valetparking-1.0/configure.php
@@ -7,14 +7,14 @@ class valetparking_1_0_0_Configure extends Bluebox_Configure
     public static $author = 'Rob Hutton';
     public static $vendor = '';
     public static $license = 'MPL';
-    public static $summary = 'Features';
+    public static $summary = 'Valet Parking';
     public static $description = 'This module configures the valet parking dialplan functionn.';
     public static $default = FALSE;
     public static $type = Package_Manager::TYPE_MODULE;
     public static $required = array(
         'core' => 0.1,
 	);
-    public static $navLabel = 'ValetParking';
+    public static $navLabel = 'Valet Parking';
     public static $navBranch = '/Applications/';
     public static $navURL = 'valetparking/index';
     public static $navSubmenu = array(
