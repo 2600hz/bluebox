913217ca4717241550da6cb73afb4f1c7a26ca8e
Rob Hutton <justlikeef@gmail.com>
2011-06-15 16:17:30 -0400

    add test to make sure that the encryption enable flag exists.

--- modules/sipencryption-1.0/libraries/drivers/freeswitch/sipencryption.php
+++ modules/sipencryption-1.0/libraries/drivers/freeswitch/sipencryption.php
@@ -6,8 +6,9 @@ class FreeSwitch_SipEncryption_Driver extends FreeSwitch_Base_Driver
     {
         if ($base instanceof SipInterface)
         {
+        	Kohana::Log('debug', print_r($base['plugins'], true));
             $xml = FreeSwitch::setSection('sofia', 'sipinterface_' . $base['sipinterface_id']);
-            $xml->update('/settings/param[@name="ssl-enable"]{@value="' . ($base['plugins']['sipencryption']['enable'] == 'TRUE' ? 'TRUE' : 'FALSE') . '"}');
+            $xml->update('/settings/param[@name="ssl-enable"]{@value="' . (isset($base['plugins']['sipencryption']) && $base['plugins']['sipencryption']['enable'] == 'TRUE' ? 'TRUE' : 'FALSE') . '"}');
             $xml->update('/settings/param[@name="tls-port"]{@value="' . $base['plugins']['sipencryption']['port'] . '"}');
             $xml->update('/settings/param[@name="tls-version"]{@value="' . $base['plugins']['sipencryption']['type'] . '"}');
             $xml->update('/settings/param[@name="ssl-dir"]{@value="' . str_replace('/', '\/', $base['plugins']['sipencryption']['certdir']) . '"}');
