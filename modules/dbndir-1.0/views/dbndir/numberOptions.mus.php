c2b9d909d2ef1e28c134ad1f33d9573691da756d
Rob Hutton <justlikeef@gmail.com>
2011-08-24 17:30:57 -0400

    dbdir: Use js template and dialplan in numberOptions.mus.php

--- modules/dbndir-1.0/views/dbndir/numberOptions.mus.php
+++ modules/dbndir-1.0/views/dbndir/numberOptions.mus.php
@@ -4,13 +4,13 @@ echo form::open_section('Options');
 	<div class="field">
 		<?php
 			echo form::label(array(
-					'for' => 'dbndir[tran_context_id]',
+					'for' => 'number{{number_id}}[dialplan][dbndir][trancontextid]',
 					'hint' => 'Context to use when transfering',
 					'help' => 'Context to transfer the calls to.'
 				),
 				'Transfer Context:'
 			);
-			echo numbering::selectContext('dbndir[tran_context_id]');
+			echo numbering::selectContext('number{{number_id}}[dialplan][dbndir][trancontextid]');
 		?>
 	</div>
 <?php 
