d01e10fd75a7cde20ec08f7382ff6c143ed68deb
Rob Hutton <justlikeef@gmail.com>
2011-12-19 12:48:06 -0500

    Adjusting conference profiles for paging to work better.

--- modules/paging-1.0/configure.php
+++ modules/paging-1.0/configure.php
@@ -29,25 +29,24 @@ class paging_1_0_0_Configure extends Bluebox_Configure
     	$confobj = new Conference();
     	$confobj->name = 'Intercom';
     	$confobj->profile= array(
-    		'rate' => '1000',
-    		'interval' => '20',
      		'energy-level' => '0',
    	   		'enter-sound' => 'tone_stream://%(200,0,500,600,700)',
-    		'exit-sound' => 'tone_stream://%(500,0,300,200,100,50,25)',
-   	   		'caller-id-name' => '$${outbound_caller_name}',
-  	   		'caller-id-number' => '$${outbound_caller_id}');
+    		'exit-sound' => '',
+    		'tts-engine' => '',
+    		'tts-voice' => ''
+    	);
   	   	$confobj->save();
   	   	
     	$confobj = new Conference();
     	$confobj->name = 'Paging';
     	$confobj->profile= array(
-    		'rate' => '1000',
-    		'interval' => '20',
      		'energy-level' => '0',
    	   		'enter-sound' => 'tone_stream://%(200,0,500,600,700)',
-    		'exit-sound' => 'tone_stream://%(500,0,300,200,100,50,25)',
-   	   		'caller-id-name' => '$${outbound_caller_name}',
-  	   		'caller-id-number' => '$${outbound_caller_id}');
+    		'exit-sound' => '',
+    		'tts-engine' => '',
+    		'tts-voice' => '',
+    		'member-flags' => 'mute'
+    	);
    	   	$confobj->save();
   	   	
   	    message::success("Default Conference profiles created...");
--- modules/paging-1.0/libraries/drivers/freeswitch/paginggroup.php
+++ modules/paging-1.0/libraries/drivers/freeswitch/paginggroup.php
@@ -17,15 +17,14 @@ class FreeSwitch_PagingGroup_Driver extends FreeSwitch_Base_Driver
         $destination = $number['Destination'];
 
         $xmlText = '        
-        <action application="set" data="api_hangup_hook=conference ' . $number['number'] . ' kick all"/>
+        <action application="set" data="api_hangup_hook=conference ' . $destination['pgg_id'] . ' kick all"/>
         <action application="answer"/>
         <action application="export" data="sip_invite_params=intercom=true"/>
         <action application="export"><![CDATA[alert_info=<sip:$${C}>;Ring;Answer]]></action>
         <action application="export"><![CDATA[sip_h_Call-Info=<sip:$${location_' . $number->location_id . '}>;answer-after=0]]></action>
-        <action application="set" data="conference_auto_outcall_caller_id_name=$\${effective_caller_id_name}"/>
-        <action application="set" data="conference_auto_outcall_caller_id_number=$\${effective_caller_id_number}"/>
+        <action application="set" data="conference_auto_outcall_caller_id_name=${effective_caller_id_name}"/>
+        <action application="set" data="conference_auto_outcall_caller_id_number=${effective_caller_id_number}"/>
         <action application="set" data="conference_auto_outcall_timeout=5"/>
-        <action application="set" data="conference_auto_outcall_flags=' . ($destination['pgg_type'] == 'page' ? 'mute' : 'none') . '"/>
         ';
 
         foreach ($destination['pgg_device_ids'] as $deviceid)
@@ -37,8 +36,8 @@ class FreeSwitch_PagingGroup_Driver extends FreeSwitch_Base_Driver
         }
         $conftype = ($destination['pgg_type'] == 'page' ? 'Paging' : 'Intercom');
         $confobj = Doctrine::getTable('Conference')->findOneByName($conftype);
-        $xmlText .= '<action application="conference" data="' . $number['number'] . '@conference_' . $confobj['conference_id'] .'"/>
-        <action application="conference" data="' . $number['number'] . ' kick all"/>
+        $xmlText .= '<action application="conference" data="' . $destination['pgg_id'] . '@conference_' . $confobj['conference_id'] .'"/>
+        <action application="conference" data="' . $destination['pgg_id'] . ' kick all"/>
         ';
         $xml->replaceWithXml($xmlText);
     }
