be843ea3325fd0bd62a226336635b1369578d311
Rob Hutton <justlikeef@gmail.com>
2012-02-20 17:19:29 -0500

    Fax align file name in spool dir for movetodir

--- modules/fax-1.0/libraries/drivers/freeswitch/movetodir.php
+++ modules/fax-1.0/libraries/drivers/freeswitch/movetodir.php
@@ -8,7 +8,7 @@ class FreeSwitch_movetodir_Driver extends Disposition_Driver
         $faxProfile = $number['Destination'];
 		
         $faxpackage = Doctrine::getTable('package')->findOneBy('name', 'fax');
-		$xml->update('/action[@application="set"][@bluebox="settingHangupHook"][@data="api_hangup_hook=' . str_replace('/', '\/', DOCROOT .  '/' . $faxpackage->basedir . '/libraries/dispositions/movetodir.php ' . $faxProfile->fxp_spool_dir . '/infax-${uuid}.tif ' . $faxProfile['registry']['destination_dir'] . '/infax-${uuid}.tif"]'));
+		$xml->update('/action[@application="set"][@bluebox="settingHangupHook"][@data="api_hangup_hook=' . str_replace('/', '\/', DOCROOT .  '/' . $faxpackage->basedir . '/libraries/dispositions/movetodir.php ' . $faxProfile->fxp_spool_dir . '/${uuid}.rxfax.tif ' . $faxProfile['registry']['destination_dir'] . '/infax-${uuid}.tif"]'));
 		
 		parent::dialplan($number);
 	}
