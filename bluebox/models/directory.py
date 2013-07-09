1e1f402906f5ad0179d9b0c2edd8e505b4efc738
frifri <francis.genet@gmail.com>
2013-07-09 14:32:12 -0700

    Starting to get how lxml work... Now able to create the xml file as I want it to be

--- bluebox/models/directory.py
+++ bluebox/models/directory.py
@@ -1,15 +1,28 @@
 from lxml import etree
 
 class Directory:
-    def create_user(self):
+    def create_user(self, etree_obj):
         # Opening the file
         target_file = open('test.xml', 'w')
 
-        # XML content
-        root = etree.Element("root")
-        root.set("interesting", "somewhat")
-        child1 = etree.SubElement(root, "test")
-        # To string
-        string_xml = etree.tostring(root, pretty_print=True)
+        # XML to string
+        string_xml = etree.tostring(etree_obj, pretty_print=True)
 
         target_file.write(string_xml)
+
+    def generate_xml(self, request):
+        # The root element here must be include since this file will be included
+        # In the directory file
+        root = etree.Element('include')
+
+        # -- user --
+        user = etree.Element('user', id='2000')
+        root.append(user)
+        # --- params ---
+        params = etree.Element('params')
+        user.append(params)
+        # ---- param ----
+        params.append(etree.Element('param', name='password', value='$${default_password}'))
+        params.append(etree.Element('param', name='vm-password', value='2000'))
+
+        return root
--- bluebox/views/home.py
+++ bluebox/views/home.py
@@ -1,10 +1,18 @@
 import json
 
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from bluebox.models import Directory
 
+@csrf_exempt
 def home(request):
+    # Creating directory object
     directory = Directory()
-    directory.create_user()
-    return HttpResponse("hello world")
+
+    if request.method == 'PUT':
+        xml = directory.generate_xml(request)
+        directory.create_user(xml)
+        return HttpResponse("Ok creation")
+
+    return HttpResponse("Fail")
