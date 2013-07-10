90e3586db957011a7c0ec5513f1b34d20d4f0954
frifri <francis.genet@gmail.com>
2013-07-10 15:05:40 -0700

    Updating base url to include directory app

--- bluebox/urls.py
+++ bluebox/urls.py
@@ -1,21 +1,5 @@
 from django.conf.urls import patterns, include, url
 
-from bluebox import views
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'bluebox.views.home', name='home'),
-    # url(r'^bluebox/', include('bluebox.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-
-    url(r'^$', views.home, name='home')
+    url(r'^account/(?P<account_id>[a-z0-9]{32})/directory/', include('bluebox.directory.urls'))
 )
