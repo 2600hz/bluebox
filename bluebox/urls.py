f07c80471c3f60da2d90b738ab777075551844bf
frifri <francis.genet@gmail.com>
2013-07-10 15:28:42 -0700

    Adding namespace for directory

--- bluebox/urls.py
+++ bluebox/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^account/(?P<account_id>[a-z0-9]{32})/directory/', include('bluebox.directory.urls'))
+    url(r'^account/(?P<account_id>[a-z0-9]{32})/directory/', include('bluebox.directory.urls', namespace='directory'))
 )
