5b577ffdd451cb0b827cc538ae195d9512b60af9
frifri <francis.genet@gmail.com>
2013-07-11 17:51:29 -0700

    Giving a try for the directory delete

--- bluebox/directory/urls.py
+++ bluebox/directory/urls.py
@@ -4,6 +4,6 @@ from bluebox.directory import views
 
 urlpatterns = patterns('',
     url(r'^create/$', views.create, name='create'),
-    url(r'^delete/$', views.delete, name='delete'),
+    url(r'^delete/[a-z0-9]{1,}$', views.delete, name='delete'),
     url(r'^edit/$', views.edit, name='edit')
 )
