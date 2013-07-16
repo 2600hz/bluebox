60126d6ebb55fc23df06b213d48944c8a85cc481
frifri <francis.genet@gmail.com>
2013-07-16 12:13:38 -0700

    dialplan urls now including extnsion urls

--- bluebox/dialplan/urls.py
+++ bluebox/dialplan/urls.py
@@ -1,9 +1,5 @@
-from django.conf.urls import patterns, inclue, urls
-
-from bluebox.dialplan import views
+from django.conf.urls import patterns, include, urls
 
 urlpatterns = patterns('',
-	url(r'^create/$', views.create, name='create'),
-    url(r'^delete/(?P<user_id>[a-z0-9]{1,}$)', views.delete, name='delete'),
-    url(r'^edit/$', views.edit, name='edit')
+	url(r'^extension/', include('bluebox.dialplan.extension.urls', namespace='extension'))
 )
