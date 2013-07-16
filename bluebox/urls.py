74b50fdcf13f15ba088e24ea692d54bb53c53ac8
frifri <francis.genet@gmail.com>
2013-07-16 12:22:36 -0700

    Update main url UUID regex

--- bluebox/urls.py
+++ bluebox/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^account/(?P<account_id>[a-f0-9]{32})/directory/', include('bluebox.directory.urls', namespace='directory')),
-	url(r'^account/(?P<account_id>[a-f0-9]{32})/dialplan/', include('bluebox.dialplan.urls', namespace='dialplan'))
+    url(r'^account/(?P<account_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/directory/', include('bluebox.directory.urls', namespace='directory')),
+	url(r'^account/(?P<account_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/dialplan/', include('bluebox.dialplan.urls', namespace='dialplan'))
 )
