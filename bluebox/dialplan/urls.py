db8e0bb02139cd8e5cc0ef33f2afd3f920fc6197
frifri <francis.genet@gmail.com>
2013-07-19 13:43:52 -0700

    Update base include for extension urls

--- bluebox/dialplan/urls.py
+++ bluebox/dialplan/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-	url(r'^extensions/', include('bluebox.dialplan.extensions.urls', namespace='extensions'))
+	url(r'^extension/', include('bluebox.dialplan.extension.urls', namespace='extension'))
 )
