930e3383bb4bd79d2ae98fddc55f8defeb5777d4
frifri <francis.genet@gmail.com>
2013-07-22 12:36:43 -0700

    Basic functions for conference configuration

--- bluebox/configuration/conference/views.py
+++ bluebox/configuration/conference/views.py
@@ -1,1 +1,20 @@
-# Create your views here.
+import json
+
+# Logger creation
+import logging
+log = logging.getLogger(__name__)
+
+from django.http import HttpResponse
+
+
+def add(request):
+    pass
+
+def list_config(request):
+    pass
+
+def edit(request):
+    pass
+
+def delete(request):
+    pass
