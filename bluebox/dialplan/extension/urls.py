7158b8b7cc75ea3b742d958f23b254e10b5d1d9e
Manu <eleaute@gmail.com>
2013-07-23 11:38:10 -0700

    Bluebox: fix merge

--- bluebox/dialplan/extension/urls.py
+++ bluebox/dialplan/extension/urls.py
@@ -4,5 +4,7 @@ from .views import *
 
 urlpatterns = patterns('',
     url(r'^create/$', create, name='create'),
-    url(r'^conference/', include('bluebox.dialplan.extension.conference.urls', namespace='conference'))
+    url(r'^conference/', include('bluebox.dialplan.extension.conference.urls', namespace='conference')),
+    url(r'^list/$', list_extension, name='extension'),
+    url(r'^voicemail/(?P<file_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/', include('bluebox.dialplan.extension.voicemail.urls', namespace='voicemail')) 
 )   
--- bluebox/dialplan/extension/views.py
+++ bluebox/dialplan/extension/views.py
@@ -1,12 +1,15 @@
 import json
+import os
 
 # Logger creation
 import logging
 log = logging.getLogger(__name__)
 
+from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
+from lxml import etree
 from bluebox.dialplan.extension.models import Extension
 
 @csrf_exempt
@@ -21,3 +24,20 @@ def create(request, account_id):
             return HttpResponse('Nahhhh, it did not create the extension')
 
     return HttpResponse('Not a PUT')
+
+def list_extension(request, account_id):
+    lists = os.listdir("%s/%s/directory/" % (settings.BLUEBOX_CONFIG_PATH, account_id))
+    list_array = {'data':{'accounts':{}}}
+
+    for file_name in lists:
+        if os.path.isdir("%s/%s/directory/%s" % (settings.BLUEBOX_CONFIG_PATH, account_id, file_name)) == False:
+            tree = etree.parse("%s/%s/directory/%s" % (settings.BLUEBOX_CONFIG_PATH, account_id, file_name))
+
+            extension = tree.find('//extension')
+            extension_name = extension.get('name')
+
+            element_dict = { "name":extension_name}
+
+            list_array['data']['accounts'][file_name[:-4]] = element_dict
+
+    return HttpResponse(json.dumps(list_array, indent=4), content_type='application/json')
