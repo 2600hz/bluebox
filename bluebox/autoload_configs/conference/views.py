5efce298b04f0b3844f98a04a759aae7ac4c1af2
Manu <eleaute@gmail.com>
2013-07-24 17:26:43 -0700

    Bluebox: list conferences paramaters

--- bluebox/autoload_configs/conference/views.py
+++ bluebox/autoload_configs/conference/views.py
@@ -1,20 +1,39 @@
 import json
+import os
 
 # Logger creation
 import logging
 log = logging.getLogger(__name__)
 
+from django.conf import settings
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
+from lxml import etree
+#from bluebox.autoload_configs.conference.models import Conference
 
 def add(request):
     pass
 
-def list_config(request):
-    pass
+def list_config(request, account_id):
+    list_profiles = {}
+    parser = etree.XMLParser(remove_comments=True)
+    tree = etree.parse("%s/%s/autoload_configs/conference_conf.xml" % (settings.BLUEBOX_CONFIG_PATH, account_id), parser)
+    profiles = tree.find('//profiles')
+
+    #element_dict = { "value":profile_value}
+
+    for profile in profiles:
+        profile_name = profile.get("name")
+        list_profiles[profile_name] = {}
+        for param in profile:
+            list_profiles[profile_name][param.get("name")] = param.get("value")
+    
+    return HttpResponse(json.dumps(list_profiles, indent=4), content_type='application/json')
 
 def edit(request):
     pass
 
 def delete(request):
     pass
+
--- bluebox/urls.py
+++ bluebox/urls.py
@@ -4,7 +4,7 @@ import views
 urlpatterns = patterns('',
     url(r'^account/(?P<account_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/directory/', include('bluebox.directory.urls', namespace='directory')),
     url(r'^account/(?P<account_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/dialplan/', include('bluebox.dialplan.urls', namespace='dialplan')),
-    url(r'^account/(?P<account_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/configuration/', include('bluebox.configuration.urls', namespace='configuration')),
+    url(r'^account/(?P<account_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/autoload_configs/', include('bluebox.autoload_configs.urls', namespace='configuration')),
 	url(r'^account/list/$', views.list, name='list'),
     url(r'^account/(?P<account_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/dialplan/extension/', include('bluebox.dialplan.extension.urls', namespace='extension'))
 )
